bb1d90b46141808d3c49da5086337e88abce489c
cyberfunk <hello@cyberfunk.org>
2013-10-03 14:56:45 +1000

    Add CORS headers to the mock server.

--- README.md
+++ README.md
@@ -4,6 +4,28 @@ This implementation must support cross domain communication and work in as many 
 
 ## Test
 
+Map the domain `xdomain.socialal.ly` in `/etc/hosts`:
+
+```
+127.0.0.1     xdomain.socialal.ly
+```
+
+And configure a reverse proxy, for example using [nginx](http://nginx.org):
+
+```
+server {
+  listen       80;
+  server_name  xdomain.socialal.ly;
+  location / {
+    proxy_redirect      off;
+    proxy_set_header    Host            $host;
+    proxy_set_header    X-Real-IP       $remote_addr;
+    proxy_set_header    X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_pass http://127.0.0.1:9080;
+  }
+}
+```
+
 1. `npm install`
 2. `npm start`
 3. `npm test` or visit `http://localhost:9080`
--- js/specs/ajax.js
+++ js/specs/ajax.js
@@ -1,34 +1,55 @@
 define(function(require) {
   var packet = {greeting: 'hello', number: 10};
+  var error = function(response, xhr) {
+    console.log('error: ' + response.status);
+  }
+  var assert = function(response, xhr) {
+    expect(response).to.have.property('status')
+      .to.be.a('number').that.equals(200);
+    expect(xhr).to.be.an('object');
+    expect(response).to.have.property('headers')
+      .to.be.an('object');
+    expect(response.headers).to.have.property('date')
+      .to.be.a('string');
+    expect(response.headers).to.have.property('content-length')
+      .to.be.a('string');
+    expect(response.headers).to.have.property('content-type')
+      .to.be.a('string').that.equals('application/json');
+    expect(response).to.have.property('data')
+      .to.be.a('string');
+    var data = JSON.parse(response.data);
+    expect(data).to.have.property('greeting')
+      .to.be.a('string').that.equals('hello');
+    expect(data).to.have.property('number')
+      .to.be.a('number').that.equals(10);
+  }
   var ajax = require('ajax');
   describe('Ajax transport', function() {
-    it('should receive echo response on same domain', function(done) {
+    it('a POST to /echo should receive echo response on same domain', function(done) {
       var success = function(response, xhr) {
-        expect(response).to.have.property('status')
-          .to.be.a('number').that.equals(200);
-        expect(xhr).to.be.an('object');
-        expect(response).to.have.property('headers')
-          .to.be.an('object');
-        expect(response.headers).to.have.property('date')
-          .to.be.a('string');
-        expect(response.headers).to.have.property('content-length')
-          .to.be.a('string');
-        expect(response.headers).to.have.property('content-type')
-          .to.be.a('string').that.equals('application/json');
-        expect(response).to.have.property('data')
-          .to.be.a('string');
-        var data = JSON.parse(response.data);
-        expect(data).to.have.property('greeting')
-          .to.be.a('string').that.equals('hello');
-        expect(data).to.have.property('number')
-          .to.be.a('number').that.equals(10);
+        assert(response, xhr);
         done();
-      }
-      var error = function(response, xhr) {
-        console.log('error: ' + response.status);
       }
       var opts = {
         url: '/echo',
+        method: 'post',
+        headers: {
+          'content-type': 'application/json'
+        },
+        data: JSON.stringify(packet),
+        success: success,
+        error: error
+      };
+      ajax(opts);
+    });
+
+    it('a POST to http://xdomain.socialal.ly/echo should receive echo response on cross domain', function(done) {
+      var success = function(response, xhr) {
+        assert(response, xhr);
+        done();
+      }
+      var opts = {
+        url: 'http://xdomain.socialal.ly/echo',
         method: 'post',
         headers: {
           'content-type': 'application/json'
