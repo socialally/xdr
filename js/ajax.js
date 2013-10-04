141795f438779b14ded13b6c46a88b9f06a4768f
cyberfunk <hello@cyberfunk.org>
2013-10-04 10:34:21 +1000

    Update client code to return an object.
    So that callers can abort the request and inspect the request.

--- js/ajax.js
+++ js/ajax.js
@@ -1,6 +1,11 @@
 ;(function() {
   "use strict";
 
+  /**
+   *  Detect Internet Explorer version as we cannot feature detect
+   *  using ('XDomainRequest' in window) as IE10 retains the
+   *  obsolete XDomainRequest object.
+   */
   var ie = (function () {
     var ret, version, browser = false, jscriptMap, jscriptVersion;
     jscriptMap = {
@@ -21,6 +26,64 @@
   }());
 
   /**
+   *  Parse response headers into an object.
+   *
+   *  @param headers The response headers as a string.
+   *
+   *  @return An object encapsulating the response headers.
+   */
+  var parse = function(headers) {
+    var output = {}, i, p, k, v;
+    headers = headers || "";
+    headers = headers.replace('\r', '');
+    headers = headers.split('\n');
+    for(i = 0;i < headers.length;i++) {
+      p = headers[i].indexOf(':');
+      k = headers[i].substr(0, p);
+      v = headers[i].substr(p + 1);
+      if(k && v) {
+        k = k.replace(/^\s+/, '').replace(/\s+$/, '');
+        v = v.replace(/^\s+/, '').replace(/\s+$/, '');
+        output[k.toLowerCase()] = v;
+      }
+    }
+    return output;
+  }
+
+  /**
+   *  Accepts the URL and query string parameters
+   *  and returns a new URL.
+   *
+   *  @param url The original URL.
+   *  @param params The query string parameters.
+   *
+   *  @return The original or updated URL.
+   */
+  var qs = function(url, params) {
+    if(!params || !(typeof(params) == 'object')) return url;
+    var u = url;
+    var q = [];
+    for(var z in params) {
+      q.push(encodeURIComponent(z) + '=' + encodeURIComponent(params[z]));
+    }
+    var qs = q.join('&');
+    u += (u.indexOf('?') == -1) ? '?' : '&';
+    u += qs;
+    return u;
+  }
+
+  /**
+   *  Converts response text info a response format.
+   *
+   *  @param text The response text.
+   */
+  var convert = function(text) {
+    //console.log("convert text: " + text);
+    //console.log("convert text: " + options.type);
+    return text;
+  }
+
+  /**
    *  Performs an ajax request.
    *
    *  @param options.method The HTTP method.
@@ -34,9 +97,10 @@
    *  @param options.error A callback for error responses.
    *  @param options.mime A MIME type passed to overrideMimeType().
    *  @param options.async Whether the request is asynchronous.
+   *  @param options.params Query string parameters to append to the URL.
    */
   var ajax = function(options) {
-    var url = options.url;
+    var url = qs(options.url || "", options.params);
     var method = options.method || ajax.defaults.method;
     var headers = options.headers || {};
     var timeout = options.timeout || ajax.defaults.timeout;
@@ -46,45 +110,13 @@
     var credentials = options.credentials || {};
     var req;
 
-    /**
-     *  Parse response headers into an object.
-     *
-     *  @param headers The response headers as a string.
-     *
-     *  @return An object encapsulating the response headers.
-     */
-    var parse = function(headers) {
-      var output = {}, i, p, k, v;
-      headers = headers || "";
-      headers = headers.replace('\r', '');
-      headers = headers.split('\n');
-      for(i = 0;i < headers.length;i++) {
-        p = headers[i].indexOf(':');
-        k = headers[i].substr(0, p);
-        v = headers[i].substr(p + 1);
-        if(k && v) {
-          k = k.replace(/^\s+/, '').replace(/\s+$/, '');
-          v = v.replace(/^\s+/, '').replace(/\s+$/, '');
-          output[k.toLowerCase()] = v;
-        }
-      }
-      return output;
-    }
-
-    var convert = function(text) {
-      //console.log("convert text: " + text);
-      //console.log("convert text: " + options.type);
-      return text;
-    }
-
     // unsupported version of ie
     if(ie.browser && ie.version < 8) {
       return false;
     }
+
     var cors = !('XDomainRequest' in window)
       || ie.browser && ie.version == 10;
-
-    //alert("use cors: " + cors);
 
     var xhr = function() {
       if(!cors) {
@@ -109,48 +141,51 @@
     }
 
     var req = xhr(), z;
-    if(req) {
-      if(!cors) {
-        req.open(method, url);
-        req.onload = function() {
-          var res = {status: this.status || 200, xhr: this, headers: null};
+    if(!req) {return false;}
+    if(!cors) {
+      req.open(method, url);
+      req.onload = function() {
+        var res = {status: this.status || 200, xhr: this, headers: null};
+        res.data = convert(this.responseText);
+        response(res);
+      };
+      req.onerror = function() {
+        var res = {status: this.status || 500, xhr: this, headers: null};
+        response(res);
+      };
+      req.ontimeout = req.onprogress = function(){};
+    }else{
+      req.open(method, url, async, credentials.username, credentials.password);
+      for(z in ajax.defaults.headers) {
+        req.setRequestHeader(z, ajax.defaults.headers[z]);
+      }
+      for(z in headers) {
+        req.setRequestHeader(z, headers[z]);
+      }
+      req.onreadystatechange = function() {
+        if(this.readyState == 4) {
+          var res = {status: this.status, xhr: this};
+          res.headers = parse(this.getAllResponseHeaders());
           res.data = convert(this.responseText);
           response(res);
-        };
-        req.onerror = function() {
-          var res = {status: this.status || 500, xhr: this, headers: null};
-          response(res);
-        };
-        req.ontimeout = req.onprogress = function(){};
-      }else{
-        //alert('using cors...');
-        req.open(method, url, async, credentials.username, credentials.password);
-        for(z in ajax.defaults.headers) {
-          req.setRequestHeader(z, ajax.defaults.headers[z]);
-        }
-        for(z in headers) {
-          req.setRequestHeader(z, headers[z]);
-        }
-        req.onreadystatechange = function() {
-          if(this.readyState == 4) {
-            var res = {status: this.status, xhr: this};
-            res.headers = parse(this.getAllResponseHeaders());
-            res.data = convert(this.responseText);
-            response(res);
-          }
         }
       }
+    }
 
-      req.timeout = timeout;
-
-      // NOTE: the setTimeout() is used due to a flaw in IE XDomainRequest
-      if(ie) {
-        setTimeout(function(){
-          req.send(options.data);
-        }, delay);
-      }else{
+    req.timeout = timeout;
+    if(ie) {
+      setTimeout(function(){
         req.send(options.data);
-      }
+      }, delay);
+    }else{
+      req.send(options.data);
+    }
+
+    return {
+      xhr: req,
+      abort: req.abort,
+      core: cors,
+      ie: ie
     }
   }
 
